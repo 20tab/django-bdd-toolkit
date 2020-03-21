49b7bc968293fd4f962eab7045f10bdc39cd0b48
Niccolò Mineo <niccolo.mineo@gmail.com>
2020-03-21 15:22:43 +0100

    Use chromedriver package name and version from requirements.txt

--- tests/environment.py
+++ tests/environment.py
@@ -7,36 +7,49 @@ from splinter.driver.webdriver.chrome import Options
 
 def before_scenario(context, scenario):
     """Prepare context for scenario."""
-    if 'browser.firefox' in scenario.tags:
-        driver_name = 'firefox'
-    elif 'browser.chrome' in scenario.tags:
-        driver_name = 'chrome'
+    if "browser.firefox" in scenario.tags:
+        driver_name = "firefox"
+    elif "browser.chrome" in scenario.tags:
+        driver_name = "chrome"
     else:
-        driver_name = getattr(settings, 'BDD_DEFAULT_BROWSER', 'chrome')
+        driver_name = getattr(settings, "BDD_DEFAULT_BROWSER", "chrome")
     params = {
-        'driver_name': driver_name,
-        'headless': getattr(settings, 'BDD_HEADLESS_BROWSER', False),
-        'incognito': getattr(settings, 'BDD_INCOGNITO_BROWSER', False),
-        'wait_time': getattr(settings, 'BDD_DEFAULT_WAIT_TIME', 5),
-        'fullscreen': getattr(settings, 'BDD_FULLSCREEN_BROWSER', False),
+        "driver_name": driver_name,
+        "headless": getattr(settings, "BDD_HEADLESS_BROWSER", False),
+        "incognito": getattr(settings, "BDD_INCOGNITO_BROWSER", False),
+        "wait_time": getattr(settings, "BDD_DEFAULT_WAIT_TIME", 5),
+        "fullscreen": getattr(settings, "BDD_FULLSCREEN_BROWSER", False),
     }
     language = {
-        'intl.accept_languages': getattr(
-            settings, 'BDD_BROWSER_LANGUAGE', 'en-US'
-        )
+        "intl.accept_languages": getattr(settings, "BDD_BROWSER_LANGUAGE", "en-US")
     }
-    if driver_name == 'firefox':
-        params.update({
-            'profile_preferences': language,
-            'capabilities': {'moz:webdriverClick': False},
-        })
-    elif driver_name == 'chrome':
-        load_entry_point('chromedriver-binary==2.43.0', 'console_scripts', 'chromedriver-path')
+    if driver_name == "firefox":
+        params.update(
+            {
+                "profile_preferences": language,
+                "capabilities": {"moz:webdriverClick": False},
+            }
+        )
+    elif driver_name == "chrome":
+        driver_package_name_constant_name = "BDD_CHROME_DRIVER_PACKAGE_NAME"
+        driver_package_name = getattr(settings, driver_package_name_constant_name, None)
+        if driver_package_name:
+            with open(
+                f"{settings.BASE_DIR}/requirements.txt", "r"
+            ) as test_packages_list:
+                for line in test_packages_list:
+                    if driver_package_name in line:
+                        load_entry_point(
+                            line, "console_scripts", "chromedriver-path",
+                        )
+        else:
+            raise Exception(
+                "Please install a chrome driver package and ensure it is "
+                f"specified in the {driver_package_name_constant_name} setting."
+            )
         options = Options()
-        options.add_experimental_option('prefs', language)
-        params.update({
-            'options': options
-        })
+        options.add_experimental_option("prefs", language)
+        params.update({"options": options})
     context.browser = Browser(**params)
 
 
